8305b52dc6eafc8709a2dc846f21f958c60eebdf
luisdamora <git@stackblitz.com>
2024-10-09 12:29:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sirio_webchat
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/luisdamora/sirio_webchat)
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './style.css';
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${logoSirio}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Sirio</h1>
+//     <h3>Estamos experimentando un salto de consciencia hacia</h3>
+//     <h3>el amanecer de la humanidad</h3>
+//     <h3>¡PREPÁRATE PARA VIVIR UNA NUEVA REALIDAD!</h3>
+//   </div>
+// `;
